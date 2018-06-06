0231129c21d50d90bde39e13c015dd9e0fb4525d
Jeff <jmschwarting@gmail.com>
2018-06-06 11:28:49 -0600

    Startup content!!!

---
+++ app.js
@@ -0,0 +1,19 @@
+$('#1').click(function() {
+    window.open("https://medium.com/s/story/crash-and-burn-how-yik-yak-fab-com-and-vine-failed-to-keep-the-momentum-going-a0f694b17053","_blank")
+});
+
+$('#2').click(function() {
+    window.open("https://one.npr.org/?sharedMediaId=559034228:559097935","_blank")
+});
+
+$('#3').click(function() {
+    window.open("http://paulgraham.com/startupideas.html","_blank")
+});
+
+$('#4').click(function() {
+    window.open("https://medium.com/swlh/7-qualities-youll-never-find-in-millionaires-b3bdf8b21843","_blank")
+});
+
+$('#5').click(function() {
+    window.open("https://youtu.be/qvHhhIfu7Lo","_blank")
+});
